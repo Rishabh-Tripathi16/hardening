b8a94e831a0309bf1a51611994293cfd9d4e8eb6
Rishabh Tripathi <2000097341@hexaware.com>
2023-11-24 17:15:04 +0530

    adding scripts

---
+++ Server2019v2.0.0/cis.bat
@@ -0,0 +1,4 @@
+cd C:\Server2019v2.0.0\
+lgpo.exe /g C:\Server2019v2.0.0\User-L1\{7C00A7EC-39B5-4AFC-AFDC-0729C1F81475}
+lgpo.exe /g C:\Server2019v2.0.0\User-L2\{CEEC7EFE-7CEC-42BF-BC95-C1C966FE86E2}
+
