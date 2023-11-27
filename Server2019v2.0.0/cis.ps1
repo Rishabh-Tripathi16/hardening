3a44c316fe86a3e46a25325c726ab21b2125baa1
Rishabh-Tripathi16 <106509115+Rishabh-Tripathi16@users.noreply.github.com>
2023-11-27 14:04:46 +0530

    Update cis.ps1

--- Server2019v2.0.0/cis.ps1
+++ Server2019v2.0.0/cis.ps1
@@ -1,2 +1,23 @@
-mkdir c:\buildArtifacts
-#cmd.exe /c C:\Server2019v2.0.0\cis.bat
+# Install IIS Website
+Install-WindowsFeature -name Web-Server -IncludeManagementTools
+ 
+# Setup directory
+New-Item C:\ImageBuilderWebApp -type Directory
+ 
+# Copy over build artifacts
+Copy-Item "C:\buildArtifacts\webApp\*" -Destination "C:\ImageBuilderWebApp" -Recurse
+ 
+# Create site
+New-IISSite -Name "ImageBuilderWebApp" -BindingInformation "*:8080:" -PhysicalPath "C:\ImageBuilderWebApp" 
+ 
+# Open firewall port for 8080
+New-NetFirewallRule -DisplayName "Allow Outbound Port 8080" -Direction Inbound -LocalPort 8080 -Protocol TCP -Action Allow
+ 
+# Clean up buildArtifacts directory
+Remove-Item -Path "C:\buildArtifacts\*" -Force -Recurse
+ 
+# Delete the buildArtifacts directory
+Remove-Item -Path "C:\buildArtifacts" -Force 
+
+cmd.exe /c C:\Server2019v2.0.0\cis.bat
+
